9f0c32ed069ac028382585b6318d7963da69cb0d
Joshua Tauberer <jt@occams.info>
2011-03-08 10:59:33 -0500

    hide feedback tab if there are no cookies, because that means we are missing the CSRF cookie and leaving a message will fail; also include a warning on submission failure

--- feedback/media/main.js
+++ feedback/media/main.js
@@ -8,7 +8,8 @@ feedback.init = function(config) {
             config.popup.addClass('loading');
 		  return true;
         },
-        'success':feedback.done(config)
+        'success':feedback.done(config),
+		'error': feedback.error(config)
     });
 
     config.button.colorbox({inline: true, href:"#"+config.popup[0].id, width: 560, height: 450, transition: "none", onComplete: function() { config.popup.removeClass('thanks'); config.popup.find('input[name=email]').focus();} })
@@ -22,5 +23,10 @@ feedback.done = function(config) {
         config.popup.find('input[name=subject]').val('');
         config.popup.delay(3000).queue( function(){ $.colorbox.close(); });
     };
+};
+feedback.error = function(config) {
+	return function (jqXHR, textStatus, errorThrown) {
+        config.popup.find('.error.hiding').removeClass("hiding").text("There was a problem submitting your feedback.");
+	};
 };
 
