f60ea3eaf59caf083680d41bfe0d0baf54b860da
Joshua Tauberer <josh@popvox.com>
2011-10-21 13:51:54 +0000

    In the admin,user field should be a raw_id_field.

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from feedback.models import Feedback
 
-admin.site.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+	raw_id_fields = ("user", )
+
+
+admin.site.register(Feedback, FeedbackAdmin)
 
 # vim: et sw=4 sts=4
